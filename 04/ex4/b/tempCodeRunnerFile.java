019e05b79be48b7f9ebbb6527555800511535e63
Duke <93232187+minhduckd5@users.noreply.github.com>
2023-11-27 21:17:52 +0700

    04
    Week 4

---
+++ 04/ex4/b/tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+        // i = 0;
+        // x1=0;
+        // y1=height;
+        // x2=5;
+        // while(i<STEPS)
+        // {   
+        //     y1-=ydelta;
+        //     x2+=xdelta;
+        //     g.drawLine(0,y1,x2,0);
+        //     g.drawLine(width,y1,x2,height);
+        //     i++;
+        // }
---
+++ 04/ex5/b/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+
+                // y = y - depth;
+                // width = width + 2 * depth;
+                // height = height + 2 * depth;
+                // g.drawArc(x, y, width, height, startAngle, arcAngle);
