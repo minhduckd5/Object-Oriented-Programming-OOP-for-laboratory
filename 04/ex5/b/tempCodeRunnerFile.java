2fd7ec92036d9dcc45c63f0d82dbe5acca5fd7fb
Duke <93232187+minhduckd5@users.noreply.github.com>
2023-11-27 21:18:16 +0700

    Delete 04 directory

--- 04/ex5/b/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-
-                // y = y - depth;
-                // width = width + 2 * depth;
-                // height = height + 2 * depth;
-                // g.drawArc(x, y, width, height, startAngle, arcAngle);
